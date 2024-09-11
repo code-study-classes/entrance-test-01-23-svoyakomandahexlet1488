f34fa781b46d513e0d26379aeff8165f7129f799
AleksandraBuxarina <honeyeddaydream@yandex.ru>
2024-09-11 13:52:45 +0300

    obj

--- hotelNames.js
+++ hotelNames.js
@@ -1,9 +1,11 @@
 import regions from "./regions";
 
-const hotelData = [
+const hotelNames = [
     { name: 'Plaza Garden Moscow WTC 5*', region: regions[1]},
     { name: 'Отель Москва 4*', region: regions[1]},
     { name: 'Cosmos St. Petersburg Pribaltiyskaya Hotel 4*', region: regions[2]},
     { name: 'Отель Санкт-Петербург 4*', region: regions[2]},
     { name: 'Сочи Парк Отель 3*', region: regions[3]},
 ];
+
+export default hotelNames;
